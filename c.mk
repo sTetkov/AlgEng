eead572d6a332061061665f671342d5add1906be
Sascha Tetkov <sascha.tetkov@uni-jena.de>
2014-11-14 20:19:20 +0100

    Added clang as compiler
    Added clang as the default compiler, added compiler specific flags.
    Added libm to the linking flags

--- c.mk
+++ c.mk
@@ -1,8 +1,8 @@
-# g++
-CXX := g++
+# clang
+CXX := clang
 LD := $(CXX)
 
-# flags
+# flags g++
 ifeq ($(CXX), g++)
 	CXX_ALL_WARNINGS_FLAG         := -Wall
 #	CXX_INFORMATIONAL_WARNINGS_FLAG := -wi
@@ -25,8 +25,36 @@ ifeq ($(CXX), g++)
 #	CXX_DOCFILE_FLAG                := -Df
 #	CXX_FPIC_FLAG                   := -fPIC
 	CXX_GTEST_LIB			:= -pthread -lgtest 
+	CXX_LINK_STDLIB			:= 
+	CXX_LINK_LIBMATH		:= -lm
 endif
 
+# flags clang
+ifeq ($(CXX), clang)
+	CXX_ALL_WARNINGS_FLAG         := -Weverything
+#	CXX_INFORMATIONAL_WARNINGS_FLAG := -wi
+#	CXX_WARNDEPRECATE_FLAG          := -dw
+	CXX_IMPORTPATH_FLAG             := -I
+	CXX_NOLINK_FLAG                 := -c
+	CXX_OUTPUTFILE_FLAG             := -o
+#	CXX_SYMBOLICDEBUGINFO_FLAG      := -g
+#	CXX_INLINE_FLAG                 := -inline
+	CXX_OPTIMIZE_FLAG               := -O3
+#	CXX_NOBOUNDSCHECK_FLAG          := -noboundscheck
+	CXX_UNITTEST_FLAG               := -DTEST_RUN
+	CXX_RELEASE_FLAG                := -DRELEASE
+	CXX_DEBUG_FLAG                  := -DDEBUG
+#	CXX_VERSION_FLAG                := -version=
+#	CXX_NOOBJECT_FLAG               := -o-
+#	CXX_STATIC_LIBRARY_FLAG         := -lib
+#	CXX_SHARED_LIBRARY_FLAG         := -shared
+#	CXX_LINKER_FLAG                 := -L
+#	CXX_DOCFILE_FLAG                := -Df
+#	CXX_FPIC_FLAG                   := -fPIC
+	CXX_GTEST_LIB			:= -pthread -lgtest 
+	CXX_LINK_STDLIB			:= -lstdc++
+	CXX_LINK_LIBMATH		:= -lm
+endif
 # implicit rules
 %.o: %.cpp
 	$(CXX) $(CXXFLAGS) $(CXX_NOLINK_FLAG) $^ $(CXX_OUTPUTFILE_FLAG)$@
